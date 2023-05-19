8ccf7e29f9e854f1bdd24dd390d1e207f076b7f0
samxoxoxo <rahibsam2@gmail.com>
2023-05-18 21:14:48 -0400

    printing output

---
+++ index.js
@@ -0,0 +1,11 @@
+// Declaring variable type string
+let first_name = "Rahib";
+let last_name = "Ahmed";
+let email = "ahme0572@algonquinlive.com";
+let output;
+
+// Creating the expression using + operator to add the varioables to the string
+output = "My name is " + first_name + " " + last_name + ". You can contact me at " + email + ".";
+
+// Display the output
+alert(output);
